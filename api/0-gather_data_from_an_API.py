0e19ae7938c0d154d734f5b8821e88a22821f86f
DELL <dell@example.com>
2026-08-17 23:34:16 +0400

    commiting

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -33,15 +33,4 @@ def main():
     employee_name = employee.get("name")
     done_tasks = [task for task in todos if task.get("completed")]
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), len(todos)
-    ))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
-
-    return 0
-
-
-if __name__ == "__main__":
-    sys.exit(main())
+    print("Employ    sys.exit(main())
